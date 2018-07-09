103206114535939a55565c56634e2a25f1566f48
Micah Shute <micah.shute@gmail.com>
2018-07-09 03:12:45 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -7,6 +7,7 @@ def create_project_hash
   projects = kickstarter.css("li.project.grid_4")
   title = projects.first.css("h2.bbcard_name strong a").text
   thumbnail = projects.first.css("div.project-thumbnail a img").attribute("src").value
+  info = projects.first.css("p.bbcard_blurb").text
   binding.pry
 end
 
