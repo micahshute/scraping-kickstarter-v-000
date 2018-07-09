e1f67bfe3e4c7ead3170289430a396714c97a1ea
Micah Shute <micah.shute@gmail.com>
2018-07-09 03:02:10 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -6,6 +6,7 @@ def create_project_hash
   kickstarter = Nokogiri::HTML(html)
   projects = kickstarter.css("li.project.grid_4")
   title = projects.first.css("h2.bbcard_name strong a").text
+  thumbnail = projects.first.css("div.project-thumbnail a img")["src"]
   binding.pry
 end
 
