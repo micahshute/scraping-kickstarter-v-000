924cccdcee5ce352abdb871232e2a3004eb70985
Micah Shute <micah.shute@gmail.com>
2018-07-09 03:00:22 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,5 +1,13 @@
-# require libraries/modules here
+require 'pry'
+require 'nokogiri'
 
 def create_project_hash
-  # write your code here
+  html = File.read("fixtures/kickstarter.html")
+  kickstarter = Nokogiri::HTML(html)
+  projects = kickstarter.css("li.project.grid_4")
+  title = projects.first.css("h2.bbcard_name strong a").text
+  binding.pry
 end
+
+
+create_project_hash
